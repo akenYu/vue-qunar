e901c0379d9a91156f43720849c5aef011823899
akenYu <251772376@qq.com>
2020-05-11 12:57:33 +0800

    add icons

---
+++ qunar/src/components/Icons.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide v-for="(page,index) of pages" :key="index">
+        <div class="icon" v-for="item in page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl" alt="icon">
+          </div>
+          <p class="icon-desc">{{ item.desc }}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HomeIcons',
+    data() {
+      return {
+        iconList: [
+          {
+          id: '01',
+          imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png',
+          desc: '酒店'
+          },
+          {
+          id: '02',
+          imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/flight.png',
+          desc: '机票'
+          },
+          {
+          id: '03',
+          imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/train.png',
+          desc: '火车票'
+          },
+          {
+          id: '04',
+          imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/package.png',
+          desc: '度假'
+          },
+          {
+          id: '05',
+          imgUrl: 'https://picbed.qunarzz.com/f5e5770393d759578962e53ee67798c8.png',
+          desc: '海外酒店'
+          },
+          {
+          id: '06',
+          imgUrl: 'https://picbed.qunarzz.com/ae617a31e0bd5803d76918b817f6d942.png',
+          desc: '自由行'
+          },
+          {
+          id: '07',
+          imgUrl: 'https://picbed.qunarzz.com/1316dc82d1ce6259686d5a68880e5a9d.png',
+          desc: '攻略'
+          },
+          {
+          id: '08',
+          imgUrl: 'https://picbed.qunarzz.com/a36d2288f19e54562338f4d8ef986288.png',
+          desc: '底价机票'
+          },
+          {
+          id: '09',
+          imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/piao.png',
+          desc: '景点门票'
+          }
+        ]
+      }
+    },
+    computed: {
+      pages() {
+        const pages = []
+        this.iconList.forEach((item, index) => {
+          const page = Math.floor(index / 8)
+          if (!pages[page]) {
+            pages[page] = []
+          }
+          pages[page].push(item)
+        })
+        return pages
+      }
+    },
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '~@/assets/styles/mixins.styl'
+  .icons >>> .swiper-container
+    height 0
+    padding-bottom 50%
+  .icon
+    position relative
+    overflow hidden
+    float left
+    width 25%
+    padding-bottom 25%
+    .icon-img
+      position absolute
+      top .1rem
+      left 0
+      right 0
+      bottom .44rem
+      box-sizing border-box
+      padding .1rem
+      .icon-img-content
+        display block
+        margin 0 auto
+        height 100%
+        width 1.4rem
+        height 1.4rem
+    .icon-desc
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      height .44rem
+      line-height .44rem
+      text-align center
+      color #333
+      ellipsis()
+</style>

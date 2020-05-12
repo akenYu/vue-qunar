c21f90fbacb082aefa8c64545cfd732b464f9913
akenYu <251772376@qq.com>
2020-05-12 23:57:23 +0800

    city ajax

--- qunar/src/views/City.vue
+++ qunar/src/views/City.vue
@@ -2,8 +2,8 @@
   <div>
     <city-header />
     <city-search />
-    <city-list />
-    <city-alphabet />
+    <city-list :cities="cities" :hot="hotCities" />
+    <city-alphabet :cities="cities" />
   </div>
 </template>
 
@@ -12,14 +12,38 @@ import CityHeader from '../components/city/Header'
 import CitySearch from '../components/city/Search'
 import CityList from '../components/city/List'
 import CityAlphabet from '../components/city/Alphabet'
+import axios from 'axios'
 
 export default {
   name: 'City',
+  data() {
+    return {
+      cities: {},
+      hotCities: []
+    }
+  },
   components: {
     CityHeader,
     CitySearch,
     CityList,
     CityAlphabet
+  },
+  methods: {
+    getCityInfo() {
+      axios.get('/api/city.json')
+        .then(this.handleGetCityInfoSucc)
+    },
+    handleGetCityInfoSucc(res) {
+      res = res.data
+      if (res.ret) {
+        const data = res.data
+        this.cities = data.cities
+        this.hotCities = data.hotCities
+      }
+    }
+  },
+  mounted() {
+    this.getCityInfo()
   }
 }
 </script>
--- qunar/src/views/Home.vue
+++ qunar/src/views/Home.vue
@@ -48,7 +48,6 @@ export default {
         this.iconList = data.iconList
         this.recommendList = data.recommendList
         this.holidayList = data.holidayList
-
       }
     }
   },

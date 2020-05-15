64b78e7eaac3a12526972730a3e86b3cf381b469
akenYu <251772376@qq.com>
2020-05-16 00:15:45 +0800

    add ticket detail

--- qunar/src/views/Detail.vue
+++ qunar/src/views/Detail.vue
@@ -2,6 +2,7 @@
   <div class="detail">
     <detail-banner />
     <detail-header />
+    <detail-list :list="list" />
     <div class="content"></div>
   </div>
 </template>
@@ -9,12 +10,34 @@
 <script>
 import DetailBanner from '../components/detail/Banner'
 import DetailHeader from '../components/detail/Header'
+import DetailList from '../components/detail/List'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
-  }
+    DetailHeader,
+    DetailList
+  },
+  data() {
+    return {
+      list: [
+        { 
+          title: '成人票',
+          children: [{
+            title: '成人票一号馆',
+            children: [{
+              title: '成人票一号馆 - 售票厅'
+            }]
+          }, {
+            title: '成人票二号馆'
+          }]
+        },
+        { title: '学生票' },
+        { title: '儿童票' },
+        { title: '特惠票' },
+      ]
+    }
+  },
 }
 </script>
 
